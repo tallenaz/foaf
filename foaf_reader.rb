2d4e8100133127d21a3946bea86a28e2a2fc6080
Anthony Zanella <azanella@stanford.edu>
2016-06-28 15:43:47 -0700

    Reading in more rdf

--- foaf_reader.rb
+++ foaf_reader.rb
@@ -6,11 +6,18 @@ puts graph.inspect
 
 query = "
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-SELECT * 
+SELECT *
 WHERE {?s foaf:knows ?o}"
 
+puts "before loading"
 sse = SPARQL.parse(query)
 sse.execute(graph) do |result|
   puts result.o
+  rdf = RDF::Resource(RDF::URI.new(result.o))
+  graph.load(rdf)
 end
 
+puts "after loading"
+sse.execute(graph) do |result|
+  puts result.o
+end
