dfb624c0f5f31ad035a79e905d31a24a80af3e8e
Anthony Zanella <azanella@stanford.edu>
2016-07-12 15:28:39 -0700

    created abstract_for(interest) method

--- foaf_reader.rb
+++ foaf_reader.rb
@@ -5,6 +5,21 @@ require 'sparql'
 require 'net/http'
 require 'openssl'
 
+def abstract_for(interest)
+  tmp_query = "PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+    PREFIX dbo: <http://dbpedia.org/ontology/>
+    SELECT ?abs
+      WHERE { ?s dbo:abstract ?abs
+        FILTER (lang(?abs) = 'en')}"
+  tmp_graph = RDF::Graph.load(interest)
+  sse_abstracts = SPARQL.parse(tmp_query)
+  sse_abstracts.execute(tmp_graph) do |res|
+    puts res.abs
+  end
+end
+
+# abstract_for('http://dbpedia.org/resource/Quilting')
+
 graph = RDF::Graph.load("me.rdf")
 
 puts graph.inspect
@@ -16,32 +31,25 @@ SELECT DISTINCT ?o
 "
 
 puts "Before loading"
-sse = SPARQL.parse(query)
-sse.execute(graph) do |result|
+sparql_query = SPARQL.parse(query)
+sparql_query.execute(graph) do |result|
   puts result.o
-  triples = RDF::Resource(RDF::URI.new(result.o))
-  graph.load(triples)
+  graph.load(result.o)
 end
 
-puts "After loading"
-sse.execute(graph) do |result|
-  puts result.o
-end
+# puts "After loading"
+# sparql_query.execute(graph) do |result|
+#   puts result.o
+# end
 
+puts 'Querying interests'
 interest_query = "
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 SELECT DISTINCT ?o
   WHERE { ?s foaf:interest ?o }
 "
-
-tmp_query = "PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-  PREFIX dbo: <http://dbpedia.org/ontology/>
-  SELECT ?abs
-    WHERE { ?s dbo:abstract ?abs
-      FILTER (lang(?abs) = 'en')}"
-
-tmp_graph = RDF::Graph.load("http://dbpedia.org/resource/Quilting")
-sse_abstracts = SPARQL.parse(tmp_query)
-sse_abstracts.execute(tmp_graph) do |res|
-  puts res.abs
+sparql_interest_query = SPARQL.parse(interest_query)
+sparql_interest_query.execute(graph) do |result|
+  puts result.o
+  abstract_for(result.o)
 end
