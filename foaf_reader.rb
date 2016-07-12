a1814b0135d1ba8713f6148280344ada69585d77
Anthony Zanella <azanella@stanford.edu>
2016-07-12 14:19:34 -0700

    Starting on abstracts query

--- foaf_reader.rb
+++ foaf_reader.rb
@@ -1,23 +1,47 @@
+# Following tutorial at http://blog.datagraph.org/2010/04/parsing-rdf-with-ruby
 require 'rdf'
 require 'linkeddata'
+require 'sparql'
+require 'net/http'
+require 'openssl'
 
 graph = RDF::Graph.load("me.rdf")
+
 puts graph.inspect
 
 query = "
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-SELECT *
-WHERE {?s foaf:knows ?o}"
+SELECT DISTINCT ?o
+  WHERE { ?s foaf:knows ?o }
+"
 
-puts "before loading"
+puts "Before loading"
 sse = SPARQL.parse(query)
 sse.execute(graph) do |result|
   puts result.o
-  rdf = RDF::Resource(RDF::URI.new(result.o))
-  graph.load(rdf)
+  triples = RDF::Resource(RDF::URI.new(result.o))
+  graph.load(triples)
 end
 
-puts "after loading"
+puts "After loading"
 sse.execute(graph) do |result|
   puts result.o
+end
+
+interest_query = "
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+SELECT DISTINCT ?o
+  WHERE { ?s foaf:interest ?o }
+"
+
+tmp_query = "PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+  PREFIX dbo: <http://dbpedia.org/ontology/>
+  SELECT ?abs
+    WHERE { ?s dbo:abstract ?abs
+      FILTER (lang(?abs) = 'en')}"
+
+tmp_graph = RDF::Graph.load("http://dbpedia.org/resource/Quilting")
+sse_abstracts = SPARQL.parse(tmp_query)
+sse_abstracts.execute(tmp_graph) do |res|
+  puts res.abs
 end
